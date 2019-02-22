38e3910ec26b644a5ec1dbab54379d412977b6d4
Christina Lavingia <christinalavingia@gmail.com>
2019-02-21 17:04:18 -0800

    Adjust curly braces around if statements.

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,35 @@
 var connection = require("../config/connection.js");
 
-var orm =
-{
-    selectAll: function(callback) {
-        connection.query("SELECT * FROM bugers", function(err, result) {
-            if (err) throw err;
-            callback (result);
+var orm = {
+    selectAll: function (callback) {
+        connection.query("SELECT * FROM burgers", function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
         });
     },
 
-    insertOne: function(burger_name, callback) {
+    insertOne: function (burger_name, callback) {
         connection.query("INSERT INTO burgers SET ?", {
             burger_name: burger_name,
             devoured: false,
-        }, function (err,result) {
-            if (err) throw err;
-            callback (result);
-        });
-    }, 
-
-    updateOne: function(burgerId, callback) {
-        connection.query("UDPATE burgers SET ? WHERE ?", [{ devoured: true}, {id: burgerId} ],
-        function (err, result) {
-            if (err) throw err;
+        }, function (err, result) {
+            if (err) {
+                throw err;
+            }
             callback(result);
         });
+    },
+
+    updateOne: function (burgerId, callback) {
+        connection.query("UDPATE burgers SET ? WHERE ?", [{ devoured: true }, { id: burgerId }],
+            function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                callback(result);
+            });
     }
 };
 
