d4a5570c0a9a176ac18db8bf95fada085ed7ec74
Christina Lavingia <christinalavingia@gmail.com>
2019-02-21 17:02:57 -0800

    Adjust orm object.

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function(callback) {
-        orm.selectAll(function(res) {
+    selectAll: function (callback) {
+        orm.selectAll(function (res) {
             callback(res);
         });
     },
-    insertOne: function(burger_name, callback) {
-        orm.insertOne(burger_name, function(res) {
+    insertOne: function (burger_name, callback) {
+        orm.insertOne(burger_name, function (res) {
             callback(res);
         });
     },
-    updateOne: function(burger_id, callback) {
-        orm.updateOne(burger_id, function(res) {
+    updateOne: function (burger_id, callback) {
+        orm.updateOne(burger_id, function (res) {
             callback(res);
         });
     }
